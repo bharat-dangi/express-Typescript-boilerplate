89bd9018ef9dc77f31fb517d8602d689282b93b2
Bharat Dangi <bharatdangi@pop-os.localdomain>
2022-11-29 13:47:05 +0545

    removed git flow

--- src/data/database/models/example.data.models.ts
+++ src/data/database/models/example.data.models.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { Example } from "../interface/example.data.interface";
 import { exampleSchema } from "../schemas/example.data.schemas";
 
-export const DistanceRouteModel = mongoose.model<Example>(
+export const ExampleModel = mongoose.model<Example>(
   "ExampleModel",
   exampleSchema
 );
